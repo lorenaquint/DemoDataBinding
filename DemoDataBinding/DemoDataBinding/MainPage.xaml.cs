9b063817b716016efce8d640e38606978a136019
lorenaquint <lorenaquint@gmail.com>
2018-04-14 00:40:25 -0500

    Initial commit

---
+++ DemoDataBinding/DemoDataBinding/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoDataBinding
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            this.btnEnviar.Clicked += BtnEnviar_Clicked;
+		}
+
+        private void BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var per = (Persona)Resources["persona1"];
+            per.Nombre = "Gabriel";
+            per.Pais = "Mexico";
+        }
+    }
+}
---
+++ DemoDataBinding/DemoDataBinding/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DemoDataBinding
+{
+    public class Persona:INotifyPropertyChanged
+    {
+        
+        private string pais;
+
+        public string Pais
+        {
+            get { return pais; }
+            set { pais = value;
+                OnPropertyChanged("Pais");
+            }
+        }
+
+
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged("Nombre");
+            }
+        }
+
+        #region Implementación
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
