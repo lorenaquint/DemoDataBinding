01e9b6463d839d14ab6b5992320dc3fe5f01f29e
lorenaquint <lorenaquint@gmail.com>
2018-04-17 15:11:13 -0500

    Modificacion

--- DemoDataBinding/DemoDataBinding/MainPage.xaml.cs
+++ DemoDataBinding/DemoDataBinding/MainPage.xaml.cs
@@ -12,23 +12,9 @@ namespace DemoDataBinding
 		public MainPage()
 		{
 			InitializeComponent();
-            //this.btnEnviar.Clicked += BtnEnviar_Clicked;
-            //this.lstPersonas.ItemSelected += LstPersonas_ItemSelected;
+           
+         
            
 		}
-
-        //private void LstPersonas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        //{
-        //    DisplayAlert(" SElecciono una persona","Sleccion","Ok");
-        //}
-
-
-
-        //private void BtnEnviar_Clicked(object sender, EventArgs e)
-        //{
-        //    var per = (Persona)Resources["persona1"];
-        //    per.Nombre = "Gabriel";
-        //    per.Pais = "Mexico";
-        //}
     }
 }
